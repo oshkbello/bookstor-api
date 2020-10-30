717d5cd0f626143e82c6ca8554bf84163824c095
Oshioke Bello <bellozeekar@gmail.com>
2020-10-29 22:45:35 -0700

    Created functions to handle login through the socialite package. Created routes to the socialAcctountController

--- membership-site/app/Http/Controllers/Auth/SocialAccountController.php
+++ membership-site/app/Http/Controllers/Auth/SocialAccountController.php
@@ -8,4 +8,30 @@ use Illuminate\Http\Request;
 class SocialAccountController extends Controller
 {
     //
+    public function redirectToProvider($provider)
+    {
+        /** Used to redirect to any social provider,
+        **  socialite handles all commuincation and authentication between 
+        *   Our app and the social provider. 
+        * Social provider grants us a access token after authentication
+        */
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        //Try block to catch any future errors from provider
+        try {  
+            //save all user information recieved from authentication 
+            $user = Socialite::driver($provider)->user();
+        } catch (Exception $e) {
+            return redirect('/login');
+        }
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+
+        Auth::login($authUser, true);
+
+        return redirect($this->redirectTo);
+    }
 }
